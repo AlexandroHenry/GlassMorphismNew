ed71b745fb59496c073eb6b6c972e305312703fa
alaxhenry <alaxhenry0121@gmail.com>
2022-12-17 22:37:34 +0900

    Initial Commit

---
+++ GlassMorphismNew/GlassMorphismNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassMorphismNewApp.swift
+//  GlassMorphismNew
+//
+//  Created by Seungchul Ha on 2022/12/17.
+//
+
+import SwiftUI
+
+@main
+struct GlassMorphismNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
